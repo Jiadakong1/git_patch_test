8d8b2e0b87e9ccbc8057168c6ec8b9f33c888396
jiaxiyang <xiyang.jia@deephi.tech>
2017-11-02 19:26:28 +0800

    test

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#**************************************************************
+#Copyright (C) 2017 DeePhi Tech. All rights reserved.
+# @Version: 1.0
+# @Created: 2017-11-02 19:25
+# @Author: Jiadakong
+# @Description:
+# @History:
+#**************************************************************
+
+echo test
